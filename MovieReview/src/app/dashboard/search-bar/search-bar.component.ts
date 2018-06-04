7434dcb6ac4e204d00e1a7a7008e73d2f1145730
avinashbailore <avinashbailore@gmail.com>
2018-06-04 23:16:27 +0530

    added searchresults dropdown

--- MovieReview/src/app/dashboard/search-bar/search-bar.component.ts
+++ MovieReview/src/app/dashboard/search-bar/search-bar.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { DashboardService } from './../dashboard.service';
-
+declare let $:any;
 @Component({
     selector: 'app-search',
     templateUrl: './search-bar.component.html',
     styleUrls: ['./search-bar.component.less']
 })
 export class SearchBarComponent implements OnInit
-{
-    public searchList:any;
+{   
 
+    public searchList:any;
     constructor(private dashboardService: DashboardService)
     {
 
@@ -18,10 +18,21 @@ export class SearchBarComponent implements OnInit
     public ngOnInit(): void
     {
         this.searchList = ['All', 'Movies', 'TV Shows'];
+
+        $('#example').focus(function() {
+          $('div.example').show();
+            $(document).bind('focusin.example click.example',function(e) {
+                if ($(e.target).closest('.example, #example').length) return;
+                $(document).unbind('.example');
+                $('div.example').fadeOut(10);
+            });  
+        });
+        $('div.example').hide();
     }
 
     public selectValue(value:any)
     {
         this.dashboardService.onSelectType(value);
     }
+
 }
